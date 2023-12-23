80b9098c0553c67cbed4ce6ca116954f3202a152
TomasCB24 <tcamerob@gmail.com>
2023-12-23 12:50:47 +0100

    FEAT(006): ADD LESSON DETAILS FIRST VERSION

--- client/src/pages/courses.tsx
+++ client/src/pages/courses.tsx
@@ -1,14 +1,26 @@
+import { Grid } from '@mui/material';
 import { Helmet } from 'react-helmet-async';
+import VideoComponent from '../components/course-lesson-details/course-lesson-details';
+import { courses } from '../_mocks/courses';
+import { useResponsive } from "../hooks/useResponsive";
 
 
 export default function CoursesPage() {
+  const isSmallScreen = useResponsive('down', 'sm');
+
   return (
     <>
       <Helmet>
         <title> Courses | FIS G4 </title>
       </Helmet>
-
-      <div> Courses Page </div>
+      <Grid container>
+      <Grid item xs={isSmallScreen ? 12 : 8}>
+          <VideoComponent playlistTitle={courses[0].title} videoTitle={courses[0].title2} videoUrl={courses[0].videoUrl} description={courses[0].description} videoImage={courses[0].image} />
+        </Grid>
+        <Grid item xs={isSmallScreen ? 12 : 4}>
+          {/* Contenido de la columna derecha */}
+        </Grid>
+      </Grid>      
     </>
-  );
+  )
 }
