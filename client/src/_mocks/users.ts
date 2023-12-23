f13ed77b83b33618e08e1f2853848af527a4fbdb
TomasCB24 <tcamerob@gmail.com>
2023-12-23 17:46:17 +0100

    FEAT(011): IMPLEMENT USER LIST WITH PAGINATION
    The first version of a list component has been created, which shows a series of users, and also a pagination component that complements it. Right now it takes all the users that exist, just missing the list of users of material X, and some more tweaks, such as styles or links.

--- client/src/_mocks/users.ts
+++ client/src/_mocks/users.ts
@@ -1,25 +1,25 @@
-import { sample } from 'lodash';
+import { sample } from 'lodash'
 
-interface User{
-  id: string;
-  photoUrl: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  email: string;
-  plan: planEnum;
-  coins: number;
+interface User {
+    id: string
+    photoUrl: string
+    firstName: string
+    lastName: string
+    username: string
+    email: string
+    plan: planEnum
+    coins: number
 }
 
-type planEnum = "Free" | "Pro" | "Premium";
+type planEnum = 'Free' | 'Pro' | 'Premium'
 
 export const users: User[] = [...Array(24)].map((_, index) => ({
-  id: (index+1).toString(),
-  photoUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
-  firstName: `Nombre de usuario ${index + 1}`,
-  lastName: `Apellido de usuario ${index + 1}`,
-  username: `username${index + 1}`,
-  email: `username${index + 1}@mail.com`,
-  plan: sample(['Free', 'Pro', 'Premium']),
-  coins: sample([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]),
-}));
+    id: (index + 1).toString(),
+    photoUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
+    firstName: `Nombre de usuarios ${index + 1}`,
+    lastName: `Apellido de usuarios${index + 1}`,
+    username: `username${index + 1}`,
+    email: `username${index + 1}@mail.com`,
+    plan: sample(['Free', 'Pro', 'Premium']),
+    coins: sample([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]),
+}))
--- client/src/pages/courses.tsx
+++ client/src/pages/courses.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from 'react-helmet-async';
+import UserList from '../components/user-list/user-list';
 
 
 export default function CoursesPage() {
@@ -7,8 +8,7 @@ export default function CoursesPage() {
       <Helmet>
         <title> Courses | FIS G4 </title>
       </Helmet>
-
-      <div> Courses Page </div>
+      <UserList />
     </>
   );
 }
