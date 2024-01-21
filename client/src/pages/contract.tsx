3316e08066dfe6bb8cc90b362e61e360126dd85a
migromarj <miguelromeroarjona@gmail.com>
2024-01-21 22:08:27 +0100

    FEAT(#003): ADD AND FORMAT ALL CA APP VERSIONS

--- client/src/pages/contract.tsx
+++ client/src/pages/contract.tsx
@@ -1,21 +1,74 @@
-import React, { useEffect, useState } from 'react';
+import { Container, ButtonGroup, Button, Typography, Grid } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from '../routes/hooks/useRouter'
 
+const cssAppliedContent = (body: any) => `
+    <div>
+      <link rel="stylesheet" type="text/css" href="/docs/CA/css/styles.css">
+      ${body}
+    </div>
+`
 
 const ContractPage: React.FC = () => {
-    const [html, setHtml] = useState<string>("");
+    const [html, setHtml] = useState<string>('')
+    const [version, setVersion] = useState<string>('v0.4')
+    const [versions, setVersions] = useState<string[]>([])
+    const router = useRouter()
 
     useEffect(() => {
-        //read html file from public folder
-        fetch('/CustomerAgreement.html')
+        const versions = ['v0.4', 'v0.5', 'v0.6', 'v0.7']
+        setVersions(versions)
+
+        fetch(`/docs/CA/Customer_Agreement_${versions[0]}.html`)
             .then((response) => response.text())
             .then((html) => {
-                setHtml(html);
-            });
-    })
+                setHtml(html)
+            })
+    }, [])
+
+    const handleVersionChange = (version: string) => {
+        fetch(`/docs/CA/Customer_Agreement_${version}.html`)
+            .then((response) => response.text())
+            .then((html) => {
+                setHtml(html)
+            })
+        setVersion(version)
+    }
 
     return (
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-    );
+        <Container maxWidth="md">
+            <Grid container justifyContent="center">
+                <Button
+                    style={{ margin: '20px', marginBottom: '40px' }}
+                    variant="contained"
+                    color="error"
+                    onClick={() => {
+                        router.push('/')
+                    }}
+                >
+                    Back to home
+                </Button>
+                <ButtonGroup style={{ margin: '20px', marginBottom: '40px' }}>
+                    {versions.map((v) => (
+                        <Button
+                            key={v}
+                            onClick={() => handleVersionChange(v)}
+                            variant={v === version ? 'contained' : 'outlined'}
+                        >
+                            {v}
+                        </Button>
+                    ))}
+                </ButtonGroup>
+            </Grid>
+            <Typography variant="h2" style={{ textAlign: 'center' }}>
+                Customer Agreement - {version}
+            </Typography>
+            <div
+                className={'html'}
+                dangerouslySetInnerHTML={{ __html: cssAppliedContent(html) }}
+            />
+        </Container>
+    )
 }
 
-export default ContractPage;
+export default ContractPage
--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -1,57 +1,54 @@
-import { lazy, Suspense } from 'react';
-import { Outlet, Navigate, useRoutes } from 'react-router-dom';
+import { lazy, Suspense } from 'react'
+import { Outlet, Navigate, useRoutes } from 'react-router-dom'
 
-import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../hooks/useAuth'
 
-import DashboardLayout from '../layouts/dashboard';
-import LandingPage from '../pages/landing';
-import LoadingView from '../sections/loading/loading';
+import DashboardLayout from '../layouts/dashboard'
+import LandingPage from '../pages/landing'
+import LoadingView from '../sections/loading/loading'
 
-export const HomePage = lazy(() => import('../pages/home'));
-export const ProfilePage = lazy(() => import('../pages/profile'));
-export const CoursesPage = lazy(() => import('../pages/courses'));
-export const PlansPage = lazy(() => import('../pages/plans'));
-export const MailboxPage = lazy(() => import('../pages/mailbox'));
-export const SettingsPage = lazy(() => import('../pages/settings'));
-export const Page404 = lazy(() => import('../pages/page-not-found'));
-export const ContractPage = lazy(() => import('../pages/contract'));
-
-
+export const HomePage = lazy(() => import('../pages/home'))
+export const ProfilePage = lazy(() => import('../pages/profile'))
+export const CoursesPage = lazy(() => import('../pages/courses'))
+export const PlansPage = lazy(() => import('../pages/plans'))
+export const MailboxPage = lazy(() => import('../pages/mailbox'))
+export const SettingsPage = lazy(() => import('../pages/settings'))
+export const Page404 = lazy(() => import('../pages/page-not-found'))
+export const ContractPage = lazy(() => import('../pages/contract'))
 
 export default function Router() {
-  const { authUser } = useAuth();
+    const { authUser } = useAuth()
 
-  const routes = useRoutes([
-    {
-      element: (
-        authUser.isAuthenticated ?
-        <DashboardLayout>
-          <Suspense fallback={<LoadingView />}>
-            <Outlet />
-          </Suspense>
-        </DashboardLayout>
-        :  
-        <LandingPage />
-      ),
-      children: [
-        { element: <HomePage />, index: true },
-        { path: 'me', element: <ProfilePage /> },
-        { path: 'courses', element: <CoursesPage /> },
-        { path: 'plans', element: <PlansPage />},
-        { path: 'mailbox', element: <MailboxPage />},
-        { path: 'settings', element: <SettingsPage /> },
-        { path: 'contract', element: <ContractPage />}
-      ],
-    },
-    {
-      path: 'error',
-      element: <Page404 />,
-    },
-    {
-      path: '*',
-      element: <Navigate to="/error" replace />,
-    },
-  ]);
+    const routes = useRoutes([
+        {
+            element: authUser.isAuthenticated ? (
+                <DashboardLayout>
+                    <Suspense fallback={<LoadingView />}>
+                        <Outlet />
+                    </Suspense>
+                </DashboardLayout>
+            ) : (
+                <LandingPage />
+            ),
+            children: [
+                { element: <HomePage />, index: true },
+                { path: 'me', element: <ProfilePage /> },
+                { path: 'courses', element: <CoursesPage /> },
+                { path: 'plans', element: <PlansPage /> },
+                { path: 'mailbox', element: <MailboxPage /> },
+                { path: 'settings', element: <SettingsPage /> },
+            ],
+        },
+        {
+            path: 'error',
+            element: <Page404 />,
+        },
+        { path: 'contract', element: <ContractPage /> },
+        {
+            path: '*',
+            element: <Navigate to="/error" replace />,
+        },
+    ])
 
-  return routes;
+    return routes
 }
