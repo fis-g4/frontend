1d3dfc11b694655e73f413e8f176128da73aec3f
migromarj <miguelromeroarjona@gmail.com>
2024-01-22 17:48:42 +0100

    FEAT(#003): ADD NEW CA VERSIONS AND FOOTER STYLES

--- client/src/pages/contract.tsx
+++ client/src/pages/contract.tsx
@@ -1,6 +1,7 @@
-import { Container, ButtonGroup, Button, Typography, Grid } from '@mui/material'
+import { Container, ButtonGroup, Button, Typography, Grid, FormControl, InputLabel, Select, SelectChangeEvent, MenuItem } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { useRouter } from '../routes/hooks/useRouter'
+import Footer from '../components/footer/footer'
 
 const cssAppliedContent = (body: any) => `
     <div>
@@ -11,63 +12,77 @@ const cssAppliedContent = (body: any) => `
 
 const ContractPage: React.FC = () => {
     const [html, setHtml] = useState<string>('')
-    const [version, setVersion] = useState<string>('v0.4')
-    const [versions, setVersions] = useState<string[]>([])
+    const [version, setVersion] = useState<string>('v0.9')
+    const versions = [
+        'v0.9',
+        'v0.8',
+        'v0.7',
+        'v0.6',
+        'v0.5',
+        'v0.4',
+    ]
     const router = useRouter()
 
     useEffect(() => {
-        const versions = ['v0.4', 'v0.5', 'v0.6', 'v0.7']
-        setVersions(versions)
-
-        fetch(`/docs/CA/Customer_Agreement_${versions[0]}.html`)
-            .then((response) => response.text())
-            .then((html) => {
-                setHtml(html)
-            })
-    }, [])
-
-    const handleVersionChange = (version: string) => {
         fetch(`/docs/CA/Customer_Agreement_${version}.html`)
             .then((response) => response.text())
             .then((html) => {
                 setHtml(html)
             })
-        setVersion(version)
+    }, [version])
+
+    const handleVersionChange = (event: SelectChangeEvent) => {
+        setVersion(event?.target.value)
     }
 
     return (
-        <Container maxWidth="md">
-            <Grid container justifyContent="center">
-                <Button
-                    style={{ margin: '20px', marginBottom: '40px' }}
-                    variant="contained"
-                    color="error"
-                    onClick={() => {
-                        router.push('/')
-                    }}
-                >
-                    Back to home
-                </Button>
-                <ButtonGroup style={{ margin: '20px', marginBottom: '40px' }}>
-                    {versions.map((v) => (
-                        <Button
-                            key={v}
-                            onClick={() => handleVersionChange(v)}
-                            variant={v === version ? 'contained' : 'outlined'}
+        <>
+            <Container maxWidth="md" style={{ marginBottom: '100px' }}>
+                <Grid container justifyContent="center">
+                    <Button
+                        style={{ margin: '20px', marginBottom: '40px' }}
+                        variant="contained"
+                        color="error"
+                        onClick={() => {
+                            router.push('/')
+                        }}
+                    >
+                        Back to home
+                    </Button>
+                    <FormControl
+                        variant="standard"
+                        sx={{ m: 1, minWidth: 120 }}
+                    >
+                        <InputLabel id="demo-simple-select-standard-label">
+                            Version
+                        </InputLabel>
+                        <Select
+                            labelId="demo-simple-select-standard-label"
+                            id="demo-simple-select-standard"
+                            value={version}
+                            onChange={handleVersionChange}
+                            label="Age"
                         >
-                            {v}
-                        </Button>
-                    ))}
-                </ButtonGroup>
-            </Grid>
-            <Typography variant="h2" style={{ textAlign: 'center' }}>
-                Customer Agreement - {version}
-            </Typography>
-            <div
-                className={'html'}
-                dangerouslySetInnerHTML={{ __html: cssAppliedContent(html) }}
-            />
-        </Container>
+                            {versions.map((v) => (
+                                <MenuItem key={v} value={v}>
+                                    {v}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Typography variant="h2" style={{ textAlign: 'center' }}>
+                    Customer Agreement - {version}
+                </Typography>
+                <div
+                    className={'html'}
+                    dangerouslySetInnerHTML={{
+                        __html: cssAppliedContent(html),
+                    }}
+                />
+            </Container>
+            <Footer />
+        </>
     )
 }
 
